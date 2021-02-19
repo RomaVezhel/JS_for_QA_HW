4315df5b61031bac6987bf551cd607da3e9a09e0
Roman.Vezhel <roman.verzhel@clicklogiq.com>
2021-02-19 22:06:18 +0200

    fixed object folder

--- JS/object.js
+++ JS/object.js
@@ -1,9 +1,8 @@
-
 //#1. Подсчет очков
 let scores = {
-    denis: 20,
-    nikita: 30
-}
+  denis: 20,
+  nikita: 30,
+};
 
 scores.denis += 10;
 scores.nikita += 30;
@@ -11,10 +10,10 @@ scores.nikita += 30;
 console.log(scores);
 
 //#2. Вглубь объектов и массивов
-var myCrazyObject = {
-    "name": "Нелепый объект",
-    "some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3],
-    "random animal": "Банановая акула"
+const myCrazyObject = {
+  name: "Нелепый объект",
+  "some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3],
+  "random animal": "Банановая акула",
 };
 
 console.log(myCrazyObject["some array"][2].number);
